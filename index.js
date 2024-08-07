d5645ac13e2fdce4f7380475868e7c8f8c5edd04
priscilla goessens <priscillagoessens@gmail.com>
2024-08-07 11:27:36 +0200

    deploy

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(express.json());
 
 // // Configure CORS
 app.use(cors({
-    origin: 'https://priscillagoessens.netlify.app/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  origin: 'https://priscillagoessens.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // API routes
 app.use("/api/projets", projetRoutes)
 
-app.listen(5000,() => {
+app.listen(10000,() => {
     connectDB();
     console.log('Listening on port ' + PORT);
 })
